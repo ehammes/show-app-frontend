a9ea6e273039dd47dad4e0db1dc694d94ed6d3a1
Katharine Swilley <kswilley@gmail.com>
2022-08-17 19:29:13 -0700

    updated user account page to replicate library page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getShows } from './Store/shows';
 import { getReviews } from './Store/reviews';
+import { getUsers } from './Store/users';
 import useShows from './Hooks/useShows';
 import useReviews from './Hooks/useReviews';
+import useUsers from './Hooks/useUsers';
 import ShowDetails from './Components/Details/ShowDetails';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -11,6 +13,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home/HomeReviews';
 import Library from './Components/Library/Library';
+import UserLibrary from './Components/Account/UserLibrary';
 
 import './App.css';
 import axios from 'axios';
@@ -21,11 +24,13 @@ function App() {
 
   const { showList, addToList } = useShows();
   const { reviewList, addToReviews } = useReviews();
+  const { userList, addToUserList } = useUsers();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getShows());
     dispatch(getReviews());
+    dispatch(getUsers());
   }, []);
 
   const handleSubmit = async (event) => {
@@ -75,16 +80,19 @@ function App() {
         </Route>
         {/* user account route */}
         <Route   
-          exact path="/account/:id"
+          exact path="/user/:id"
           element= {
            <>
-           {/* <Account? UserLibrary? /> */}
+           <UserLibrary />
            </>
          }>
          </Route>
       </Routes>
     </BrowserRouter>
+
+    <UserLibrary />
     </div>
+
     // <div className="App">
     //   <Header />
     //   <form onSubmit={handleSubmit}>
